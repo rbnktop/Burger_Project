f515405f59c10fb7fda4bad25fc16e32fb46a68f
victor <vitorh2606@gmail.com>
2026-05-09 21:46:56 -0300

    feat(inventory): expose view names

--- PBurger/Inventory/urls.py
+++ PBurger/Inventory/urls.py
@@ -12,6 +12,9 @@ urlpatterns = [
     path(
         "Inventario/Apagar/<int:stock_id>", views.delete_stock_item_view, name="apagar"
     ),
+
+
+
     path("Receita/", views.list_recipe_view, name="receita_inventario"),
     path("Receita/Criar", views.create_recipe_view, name="receita_criar"),
     path(
@@ -24,6 +27,9 @@ urlpatterns = [
         views.delete_recipe_view,
         name="receita_apagar",
     ),
+
+
+
     path("Produto/", views.list_product_view, name="produto_inventario"),
     path("Produto/Criar", views.create_product_view, name="produto_criar"),
     path(
@@ -36,4 +42,5 @@ urlpatterns = [
         views.delete_product_view,
         name="produto_apagar",
     ),
+    
 ]
