bf496fa79644da8a8b2b7a7cf8b377d3018c8699
victor <vitorh2606@gmail.com>
2026-05-12 05:24:35 -0300

    fix(layout): improve ui design

--- PBurger/Greeting/urls.py
+++ PBurger/Greeting/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home_view, login_view, logout_view
+from .views import home_view, login_view, logout_view, history_sidebar
 
 app_name = "greeting"
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", home_view, name="home"),
     path("login/", login_view, name="login"),
     path("logout/", logout_view, name="logout"),
+    path('history-sidebar/', history_sidebar, name='history_sidebar'),
 ]
